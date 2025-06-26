829523c9c9342d70bd62e88346503be5b6aecaea
Edvin Dunaway <git@eddinn.net>
2025-06-26 16:45:58 +0200

    style overhaul in favour of bootstrap

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         Object.entries(serviceData).forEach(([group, urls], groupIndex) => {
             const groupCol = document.createElement("div");
-            groupCol.className = "group-card";
+            groupCol.className = "col-md-6 group-card";
             groupCol.dataset.group = group;
 
             const groupCard = document.createElement("div");
